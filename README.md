e58e3ae3cecab65dc03854623ff8e65cc122ba19
IzayoiRin <izy@google.com>
2022-06-19 18:27:53 +0800

    add exception and other lazy settings

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # tari
+
 Target IDF
+
+## Environment
+
+### Set config from Env
+
+- Env configure key:
+
+`VERITAS_APPLICATION_SETTINGS`
+
+- Support config protocols:
+
+| Protocol | Pattern             | Example                       |
+|----------|---------------------|-------------------------------|
+| Module   | module:module-path  | `moudel:conf.settings`        |
+| Json     | json:json-file-path | `moudel:./conf/settings.json` |
+| URL      | url:setting-url     | `url:http://conf/settings`    |
--- tari/manager.py
+++ tari/manager.py
@@ -2,9 +2,19 @@ import os
 
 
 def main():
+    """
+    runserver entrance
+    env configure key:
+        VERITAS_APPLICATION_SETTINGS
+
+    support config protocol:
+        - module: "module:conf.settings"
+        - url: "url:http://conf/settings"
+        - json: "json:./conf/settings.json"
+    """
     from veritas.builtin.const import ENVIRONMENT_KEY
     if os.environ.get(ENVIRONMENT_KEY) is None:
-        os.environ.setdefault(ENVIRONMENT_KEY, "conf.settings")
+        os.environ.setdefault(ENVIRONMENT_KEY, "module:conf.settings")
 
     from app.application import get_application
     app = get_application("tari")
